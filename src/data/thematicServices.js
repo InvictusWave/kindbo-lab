c14ab642c8164c688cc673a4f9de2830dc938013
adamibnu <adamibnu157@gmail.com>
2025-10-26 19:42:45 +0700

    feat(home): fix service data dummy

---
+++ src/data/thematicServices.js
@@ -0,0 +1,51 @@
+
+export const steps = [
+    {
+        title: "Mini Chef Day",
+        content: "Jelajahi dunia rasa dan tekstur! Anak belajar memasak sambil mengenal bahan makanan, bekerja sama, dan menikmati hasil kreasinya sendiri.",
+        image: "assets/images/course/01.jpg",
+        link: "#"
+    },
+    {
+        title: "Little Gardener",
+        content: "Menanam, merawat, dan memanen dengan tangan mereka sendiri. Belajar mencintai alam dan memahami proses tumbuh dari biji hingga kehidupan.",
+        image: "assets/images/course/02.jpg",
+        link: "#"
+    },
+    {
+        title: "Craft and Create",
+        content: "Tempat ide menjadi nyata! Anak bebas berkreasi dengan warna, bentuk, dan bahan sederhana untuk mengekspresikan imajinasinya.",
+        image: "assets/images/course/03.jpg",
+        link: "#"
+    },
+    {
+        title: "Science Spark",
+        content: "Eksperimen seru yang memantik rasa ingin tahu. Anak mengenal sains lewat permainan sederhana yang bikin “wow” dan penuh penemuan.",
+        image: "assets/images/course/04.jpg",
+        link: "#"
+    },
+    {
+        title: "Cultural Play",
+        content: "Berkenalan dengan dunia lewat permainan, cerita, dan tradisi dari berbagai negara. Mengajarkan empati, keberagaman, dan rasa ingin tahu global.",
+        image: "assets/images/course/05.jpg",
+        link: "#"
+    },
+    {
+        title: "Nature Hunt",
+        content: "Petualangan kecil di alam terbuka! Anak diajak mengeksplorasi sekitar, mengenal makhluk hidup, dan belajar lewat observasi langsung.",
+        image: "assets/images/course/06.jpg",
+        link: "#"
+    },
+    {
+        title: "Sensory Journey",
+        content: "Aktivitas yang merangsang seluruh pancaindra—melihat, menyentuh, mencium, mendengar, dan merasakan. Membantu anak lebih fokus dan percaya diri.",
+        image: "assets/images/course/07.jpg",
+        link: "#"
+    },
+    {
+        title: "Story & Sound",
+        content: "Cerita yang hidup lewat irama dan suara! Anak mendengar, bergerak, dan berimajinasi sambil membangun bahasa dan emosi positif.",
+        image: "assets/images/course/08.jpg",
+        link: "#"
+    }
+];
